5cc35e068ee47cb0babe78121a7bff9d8d36dbda
Furkan <label2589@gmail.com>
2021-01-02 22:57:56 +0300

    Saturday Night (10:57)

--- scripts.js
+++ scripts.js
@@ -4,3 +4,10 @@ function myFunction() {
 function blablabla() {
     document.getElementById("demo").innerHTML = "Bla Bla Bla"
 }
+
+document.getElementById('changeBGC').onclick=function(){
+    document.getElementById('body').style.backgroundColor="rgb(13, 16, 23)"; /*github's bgcolor*/
+    
+}
+
+
