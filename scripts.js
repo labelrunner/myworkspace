29cae4a216c39a9c724e9dd8e3b9e3e9071f18f6
Furkan <label2589@gmail.com>
2021-01-02 20:51:40 +0300

    Saturday Night

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Changing paragraphs with js codes"
+}
+function blablabla() {
+    document.getElementById("demo").innerHTML = "Bla Bla Bla"
+}
