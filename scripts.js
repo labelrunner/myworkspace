c9e568e0be51496a9e4bf1130a962576642ef84e
Furkan <label2589@gmail.com>
2021-01-07 16:27:13 +0300

    7th of Jan

--- scripts.js
+++ scripts.js
@@ -15,3 +15,20 @@ alert("Welcome to My Workspace!");
 
 console.trace(2+2); /* let us trace the src of the specific code */
 
+var i=0;
+var images = ['images/black.jpg','images/fire.jpeg','images/lightning.jpg','images/water.jpg'];
+
+var duration = 2000;
+function slideImg(){
+    document.slide.src = images[i];
+
+    if(i < images.length - 1){
+        i++;
+    }
+    else{
+        i=0;
+    }
+
+    setTimeout("slideImg()", duration);
+}
+window.onload = slideImg;
